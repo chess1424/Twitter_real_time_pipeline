38ff3c61b1969468441e96e0b6980387dab602a7
Carlos Martinez <carlosmartinez@cmartinez-MacBook-Pro.local>
2018-02-08 02:55:56 -0600

    Fixed sh file execution

--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -1,6 +1,5 @@
 #start zookeper
-$KAFKA_HOME/bin/zookeper-server-start.sh $KAFKA_HOME/config/zookeper.properties &
-
+$KAFKA_HOME/bin/zookeper-server-start.sh $KAFKA_HOME/config/zookeper.properties
 
 #start kafka broker
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
