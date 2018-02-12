566aaeebb2d5f8aabee0a26ed49ec010c5321ed4
Carlos Martinez <carlos.1424@live.com.mx>
2018-02-12 03:21:39 -0600

    Create twitter stream topic

--- kafka/create-kafka-topics.sh
+++ kafka/create-kafka-topics.sh
@@ -6,4 +6,5 @@ while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'/ {exit 1}'; do
 done
 
 #Able to create a kafka topic
+echo "Creating kafka topics"
 $KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic twitter-streamer
--- kafka/start-kafka.sh
+++ kafka/start-kafka.sh
@@ -3,3 +3,6 @@ $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.propertie
 
 #start kafka broker
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+
+#create kafka topics
+create-kafka-topics.sh
