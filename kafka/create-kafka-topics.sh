e30df292f33c44c614e3d2d207082b1d99cc11ee
Carlos Martinez <carlos.1424@live.com.mx>
2018-02-12 02:26:06 -0600

    Create kafka topic with docker automatically

---
+++ kafka/create-kafka-topics.sh
@@ -0,0 +1,9 @@
+KAFKA_PORT="2181"
+
+while netstat -lnt | awk '$4 ~ /:'$KAFKA_PORT'/ {exit 1}'; do
+	echo "waiting for kafka to be ready"
+	sleep 10;
+done
+
+#Able to create a kafka topic
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic twitter-streamer
