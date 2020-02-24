56db619163c4756b3a32a93a55bcda1e96e3956a
Carlos Martinez <carlos.1424@live.com.mx>
2020-02-24 02:32:19 -0600

    Fixed too many zookeeper connections

--- Spark_Streaming_Consumer/build.sbt
+++ Spark_Streaming_Consumer/build.sbt
@@ -8,8 +8,8 @@ scalaVersion := "2.11.0"
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-streaming" % "1.6.2",
   "org.apache.spark" %% "spark-streaming-kafka" % "1.6.2",
-  "org.apache.spark" %% "spark-sql" % "1.6.2",
-  "org.apache.kafka" %% "kafka" % "0.10.1.0",
+  //"org.apache.spark" %% "spark-sql" % "1.6.2",
+  //"org.apache.kafka" %% "kafka" % "0.10.1.0",
   "io.druid" %% "tranquility-spark" % "0.8.3" excludeAll(
     ExclusionRule(organization = "com.sun.jersey")
   )
--- Twitter_client/src/main/scala/twitter/client/TwitterListener.scala
+++ Twitter_client/src/main/scala/twitter/client/TwitterListener.scala
@@ -3,16 +3,20 @@ package twitter.client
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import twitter4j.{StallWarning, Status, StatusDeletionNotice, StatusListener}
 import twitter.Tweet
+import scala.util.matching.Regex
 
 class TwitterListener(producer: KafkaProducer[String, Tweet], topic: String, key: String) {
 
   def simpleStatusListener = new StatusListener() {
     def onStatus(status: Status) {
-      var countryCode:String = null
+      var countryCode:String = "Unknown"
       if(status.getPlace != null && status.getPlace.getCountryCode != null)
         countryCode = status.getPlace.getCountryCode
 
-      val tweet  = new Tweet(countryCode, status.getSource, status.getUser.getId,
+      val keyValPattern: Regex =  """(?<=nofollow\\">)(.*)(?=</a)""".r
+      val source = keyValPattern.findFirstIn(status.getSource).orElse(Option("Unknown")).get
+
+      val tweet  = new Tweet(countryCode, source, status.getUser.getId,
         status.isRetweet, status.getText)
 
       val record = new ProducerRecord[String, Tweet](topic,key,tweet)
