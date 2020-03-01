ffd52c10e29f948e5170a600aaac1e5782499738
Carlos Martinez <carlos.1424@live.com.mx>
2020-02-29 20:11:45 -0600

    Added more dimensions and changed window perior to 5 minutes

--- Spark_Streaming_Consumer/src/main/scala/druid/Message.scala
+++ Spark_Streaming_Consumer/src/main/scala/druid/Message.scala
@@ -5,23 +5,22 @@ import com.github.nscala_time.time.Imports._
 import com.metamx.common.scala.untyped._
 import com.metamx.tranquility.typeclass.Timestamper
 
-
-
-case class Message(time: DateTime, userId: String, countryCode: String, source: String, attachedLinks: String,
-                   hashTags: String, words: Int, isVerified: Boolean, isRetweet: Boolean, isPossibleSensitive: Boolean,
+case class Message(time: DateTime, userId: String, tweetId: String, countryCode: String, source: String, attachedLinks: String,
+                   hashTags: String, userVerifiable: String, retweetStatus: String, sensitivity: String, words: Int,
                    timesRetweeted: Long, followers: Int, friends: Int){
   @JsonValue
   def toMap: Map[String, Any] = Map(
     "timestamp" -> (time.getMillis / 1000),
     "userId" -> userId,
+    "tweetId" -> tweetId,
     "countryCode" -> countryCode,
     "source" -> source,
     "attachedLinks" -> attachedLinks,
     "hashTags" -> hashTags,
+    "userVerifiable" -> userVerifiable,
+    "retweetStatus" -> retweetStatus,
+    "sensitivity" -> sensitivity,
     "words" -> words,
-    "isVerified" -> isVerified,
-    "isRetweet" -> isRetweet,
-    "isPossibleSensitive" -> isPossibleSensitive,
     "timesRetweeted" -> timesRetweeted,
     "followers" -> followers,
     "friends" -> friends
@@ -39,14 +38,15 @@ object Message {
     Message(
       new DateTime(long(d("timestamp")) * 1000),
       str(d("userId")),
+      str(d("tweetId")),
       str(d("countryCode")),
       str(d("source")),
       str(d("attachedLinks")),
       str(d("hashTags")),
+      str(d("userVerifiable")),
+      str(d("retweetStatus")),
+      str(d("sensitivity")),
       int(d("words")),
-      bool(d("isVerified")),
-      bool(d("isRetweet")),
-      bool(d("isPossibleSensitive")),
       long(d("timesRetweeted")),
       int(d("followers")),
       int(d("friends"))
--- Tweet_serializer_deserializer/src/main/scala/twitter/Tweet.scala
+++ Tweet_serializer_deserializer/src/main/scala/twitter/Tweet.scala
@@ -4,59 +4,64 @@ import java.io.Serializable
 
 class Tweet extends Serializable{
   private var userId: String = null
+  private var tweetId: String = null
   private var countryCode: String = null
   private var source: String = null
   private var attachedLinks: String = null
   private var hashTags: String = null
+  private var userVerifiable: String = null //verified, not verified
+  private var retweetStatus: String = null //retweeted, not retweeted
+  private var sensitivity: String = null //sensible, not sensible
   private var words: Int = 0
-  private var isVerified: Boolean= false
-  private var isRetweet: Boolean = false
-  private var isPossibleSensitive: Boolean = false
   private var timesRetweeted: Long = 0
   private var followers: Int = 0
   private var friends: Int = 0
 
-  def this(userId: String, countryCode: String, source: String, attachedLinks: String, hashTags: String,
-           words: Int, isVerified: Boolean, isRetweet: Boolean, isPossibleSensitive: Boolean, timesRetweeted: Long,
-           followers: Int, friends: Int){
+  def this(userId: String, tweetId: String, countryCode: String, source: String, attachedLinks: String, hashTags: String,
+           userVerifiable: String, retweetStatus: String, sensitivity: String, words: Int, timesRetweeted: Long, followers: Int,
+           friends: Int){
     this()
     this userId = userId
+    this tweetId = tweetId
     this countryCode = countryCode
     this source = source
     this attachedLinks = attachedLinks
     this hashTags = hashTags
+    this userVerifiable = userVerifiable
+    this retweetStatus = retweetStatus
+    this sensitivity = sensitivity
     this words = words
-    this isVerified = isVerified
-    this isRetweet = isRetweet
-    this isPossibleSensitive = isPossibleSensitive
     this timesRetweeted = timesRetweeted
     this followers = followers
     this friends = friends
   }
 
   def getUserId: String = this.userId
+  def getTweetId: String = this.tweetId
   def getCountryCode: String = this.countryCode
   def getSource: String = this.source
   def getAttachedLinks: String = this.attachedLinks
   def getHashTags: String = this.hashTags
+  def getUserVerifiable: String = this.userVerifiable
+  def getRetweetStatus: String = this.retweetStatus
+  def getSensitivity: String = this.sensitivity
   def getWords: Int = this.words
-  def getIsVerified: Boolean = this.isVerified
-  def getIsRetweet: Boolean = this.isRetweet
-  def getIsPossibleSensitive: Boolean = this.isPossibleSensitive
   def getTimesRetweeted: Long = this.timesRetweeted
   def getFollowers: Int = this.followers
   def getFriends: Int = this.friends
 
   override def toString: String =
-    s"""userId = $userId
+    s"""
+       | userId = $userId
+       | tweetId = $tweetId
        | countryCode = $countryCode
        | source =  $source
        | attachedLinks = $attachedLinks
        | hashTags = $hashTags
+       | userVerifiable = $userVerifiable
+       | retweetStatus = $retweetStatus
+       | sensitivity = $sensitivity
        | words = $words
-       | isVerified = $isVerified
-       | isRetweet = $isRetweet
-       | isPossibleSensitive = $isPossibleSensitive
        | timesRetweeted = $timesRetweeted
        | followers = $followers
        | friends = $friends
